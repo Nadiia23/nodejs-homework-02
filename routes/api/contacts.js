d045997af500949582203a19256ed913fe12c2c7
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-12-19 17:41:21 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -96,7 +96,7 @@ router.patch("/:contactId/favorite", async (req, res, next) => {
     const { contactId } = req.params;
     const { error } = updateFavoriteSchema.validate(req.body);
     if (error) {
-      throw createError(400, error.message);
+      throw createError(400, {message: "missing field favorite"});
     }
     const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
     if (!result) {
